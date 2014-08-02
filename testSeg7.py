7fe3b6bcd50befa63e8aed938a13c471f9912187
whaleygeek <david@thinkingbinaries.com>
2014-08-02 17:07:25 +0100

    Changed default display polarity to Common Anode
    Most displays on the market these days are Common Anode, so it makes sense for that to be the default.

--- testSeg7.py
+++ testSeg7.py
@@ -1,16 +1,19 @@
-# Adventure 5: testDisplay2.py
+# testSeg7.py - Test a 7-segment display
 
 import anyio.seg7 as display
 import time
 
+# Use this for Raspberry Pi
 #import RPi.GPIO as GPIO
 #LED_PINS = [10,22,25,8,7,9,11,15]
+
+# Use this for Arduino
 import anyio.GPIO as GPIO
 LED_PINS = [7,6,14,16,10,8,9,15]
 
 GPIO.setmode(GPIO.BCM)
 
-ON = True # common-anode. Set to True for a common cathode display
+ON = False # common-anode. Set to True for a common cathode display
 
 display.setup(GPIO, LED_PINS, ON)
 
